f61412fcf252ff298582346771d32002fabf929b
CharlieBeck <chuckb1789@gmail.com>
2017-05-31 17:02:09 -0600

    [UPDATE]: Update file structure and beautify.

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
-const images = ['./mtn1.jpg', './mtn2.jpg'];
+var images = ['./mtn1.jpg', './mtn2.jpg'];
 
-const count = 0;
+var count = 0;
 
-function mountains () {
-  count++ if (count % 2 == 0) {
+function mountains() {
+  count++
+  if (count % 2 == 0) {
     $('#mtn').attr("src", images[0]);
   } else {
     $('#mtn').attr("src", images[1]);
